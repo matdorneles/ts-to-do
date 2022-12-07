6ca2dc9adf86eac835bd1e3ac8aa5edd3778b658
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-12-06 21:24:43 -0300

    improv: removed unnecessary code

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv'
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import "express-async-errors";
 import cors from "cors";
 import { router } from "./routes";
@@ -8,23 +8,10 @@ const app = express();
 
 dotenv.config();
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if(err instanceof Error) {
-        return res.status(400).json({
-            error: err.message
-        });
-    }
-    
-    return res.status(500).json({
-        status: "error",
-        messge: "Internal server error"
-    });
-});
-
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
 const port = 8080;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
